de8a79ef00cc1dd70c570830b808f9814200c479
JuKu <kuenzel.justin@t-online.de>
2018-03-04 17:36:55 +0100

    added new exceptions for database table upgrader.

---
+++ system/core/exception/unsupporteddatatypeexception.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Exception which is thrown, if mysql data type isnt supported
+ */
+class UnsupportedDataTypeException extends Exception {
+
+}
---
+++ system/core/exception/unsupporteddbengineexception.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Exception which is thrown, if database engine isnt supported
+ */
+class UnsupportedDBEngineException extends Exception {
+
+}
