029bf15704d3e214028da43cff2c06fdbe593055
JuKu <kuenzel.justin@t-online.de>
2018-03-10 00:32:56 +0100

    changed class PHPUtils, added support to detect if mod_rewrite is available.

--- system/core/classes/phputils.php
+++ system/core/classes/phputils.php
@@ -10,4 +10,16 @@ class PHPUtils {
         return class_exists('Memcached');
     }
 
+    public static function isModRewriteAvailable () {
+    	if (function_exists("apache_get_modules")) {
+    		if (in_array('mod_rewrite',apache_get_modules())) {
+    			return true;
+			}
+
+			return false;
+		}
+
+		return false;
+	}
+
 }
