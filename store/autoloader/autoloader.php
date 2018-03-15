168d8fd0c3fd00d30e7484b8eb5acc366636c673
JuKu <kuenzel.justin@t-online.de>
2018-03-15 16:02:19 +0100

    added class User to class autoloader.

--- store/autoloader/autoloader.php
+++ store/autoloader/autoloader.php
@@ -42,6 +42,7 @@ $autoloader_classes = array(
 	"system/packages/com.jukusoft.cms.page/classes/page.php",
 	"system/packages/com.jukusoft.cms.page/classes/pagetype.php",
 	"system/packages/com.jukusoft.cms.htmlpage/classes/htmlpage.php",
+	"system/packages/com.jukusoft.cms.user/classes/user.php"
 );
 
 ?>
