ab609e6db0cf53a173a67ae06dc11401e02517aa
JuKu <kuenzel.justin@t-online.de>
2018-03-30 13:19:55 +0200

    added composer setup.

---
+++ twig_performance/composer_setup.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright (c) 2018 Justin Kuenzel (jukusoft.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * Installer for composer
+ *
+ * https://getcomposer.org/download/
+ *
+ * https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+ */
+
+copy('https://getcomposer.org/installer', 'composer-setup.php');
+if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;
+
+//install composer
+require("composer-setup.php");
+
+//remove composer setup
+unlink('composer-setup.php');
+
+?>
