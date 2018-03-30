6a88d873246b8e39036dbc2c877a9f5c5cdebba3
JuKu <kuenzel.justin@t-online.de>
2018-03-30 16:49:50 +0200

    changed twig performance.

--- twig_performance/index.php
+++ twig_performance/index.php
@@ -61,7 +61,11 @@ $template->addGlobal("COPYRIGHT", "Copyright (c) 2018 JuKuSoft.com");
 $template->addGlobal("VERSION", "1.0.0");
 $template->addGlobal("BUILD", "1001");
 
-echo $template->render(array('name' => 'Fabien'));
+echo $template->render(array(
+	'name' => 'Fabien',
+	'title' => "My title",
+	'charset' => "UTF-8"
+));
 
 $end_time = microtime(true);
 $exec_time = $end_time - $start_time;
