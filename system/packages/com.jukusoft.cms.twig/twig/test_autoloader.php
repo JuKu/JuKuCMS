0a67be4ff8c6826f9638647b25c20a28c85cbf63
JuKu <kuenzel.justin@t-online.de>
2018-03-30 15:58:44 +0200

    fixed twig autoloader.

--- system/packages/com.jukusoft.cms.twig/twig/test_autoloader.php
+++ system/packages/com.jukusoft.cms.twig/twig/test_autoloader.php
@@ -34,6 +34,8 @@ class Twig_Autoloader
 		}
 		if (is_file($file = dirname(__FILE__).'/'.str_replace(array('_', "\0"), array('/', ''), $class).'.php')) {
 			require $file;
+		} else {
+			echo "Cannot load twig class: " . $class . ", expected path: " . dirname(__FILE__).'/'.str_replace(array('_', "\0"), array('/', ''), $class).'.php' . " .";
 		}
 	}
 }
