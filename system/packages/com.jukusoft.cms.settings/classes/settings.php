53894bf80d72bfccd86ec01b6895de9ef11a3d97
JuKu <kuenzel.justin@t-online.de>
2018-04-02 16:00:42 +0200

    fixed some problems in class Settings, fixed a bug which creates 3 new menus on every database upgrade.

--- system/packages/com.jukusoft.cms.settings/classes/settings.php
+++ system/packages/com.jukusoft.cms.settings/classes/settings.php
@@ -86,6 +86,9 @@ class Settings {
 	 * @return true if setting key exists
 	 */
 	public static function contains (string $key) : bool {
+		//load settings if neccessary
+		self::loadSettingsIfAbsent();
+
 		//escape key
 		$key = Database::getInstance()->escape($key);
 
