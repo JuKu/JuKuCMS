aa7877a230638ea2dc75f3e4067f03b2e9488458
JuKu <kuenzel.justin@t-online.de>
2018-04-03 20:33:07 +0200

    fixed some problems in dwoo performance benchmark.

--- twig_performance/dwoo.php
+++ twig_performance/dwoo.php
@@ -60,6 +60,7 @@ define('DWOO_PATH', ROOT_PATH . "system/packages/com.jukusoft.cms.dwoo/dwoo/lib/
 
 require(DWOO_PATH . "Core.php");
 require(DWOO_PATH . "Template/File.php");
+require(DWOO_PATH . "Template/Str.php");
 
 // Create the controller, it is reusable and can render multiple templates
 $core = new Dwoo\Core();
