285830e65a679d3a651611238a836c196cb7c730
JuKu <kuenzel.justin@t-online.de>
2018-04-07 23:07:20 +0200

    fixed some problems in plugins page.

--- system/packages/com.jukusoft.cms.plugin/classes/pluginspage.php
+++ system/packages/com.jukusoft.cms.plugin/classes/pluginspage.php
@@ -38,7 +38,15 @@ class PluginsPage extends PageType {
 			<script>
 				$(function () {
 					$('#example1').DataTable();
-				})
+					$('#example2').DataTable({
+						'paging'      : true,
+						'lengthChange': false,
+					 	'searching'   : false,
+					 	'ordering'    : true,
+					 	'info'        : true,
+					 	'autoWidth'   : false
+					});
+				});
 			</script>";
 	}
 
