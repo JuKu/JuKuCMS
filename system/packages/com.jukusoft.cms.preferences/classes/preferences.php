25512266be2d6efa3fdb4c061cc80da797e2d12f
JuKu <kuenzel.justin@t-online.de>
2018-04-16 21:55:07 +0200

    added support for preferences, closed #139 .

--- system/packages/com.jukusoft.cms.preferences/classes/preferences.php
+++ system/packages/com.jukusoft.cms.preferences/classes/preferences.php
@@ -27,21 +27,88 @@
 
 class Preferences {
 
+	protected $area = "";
 	protected $prefs = array();
+	protected $changed_prefs = array();
 
-	public function __construct(string $name) {
-		if (Cache::contains("preferences", "preferences-" . $name)) {
-			$this->prefs = Cache::get("preferences", "preferences-" . $name);
+	public function __construct(string $area) {
+		if (!PHPUtils::startsWith($area, "plugin_") && !PHPUtils::startsWith($area, "style_")) {
+			throw new IllegalArgumentException("preferences area name should start with 'plugin_' or 'style_'´.");
+		}
+
+		$this->area = $area;
+
+		if (Cache::contains("preferences", "preferences-" . $area)) {
+			$this->prefs = Cache::get("preferences", "preferences-" . $area);
 		} else {
-			$rows = Database::getInstance()->listRows("SELECT * FROM `{praefix}preferences` WHERE `area` = :area; ", array('area' => $name));
+			$rows = Database::getInstance()->listRows("SELECT * FROM `{praefix}preferences` WHERE `area` = :area; ", array('area' => $area));
 
 			foreach ($rows as $row) {
 				$this->prefs[$row['key']] = unserialize($row['value']);
 			}
 
 			//cache preferences
-			Cache::put("preferences", "preferences-" . $name, $this->prefs);
+			Cache::put("preferences", "preferences-" . $area, $this->prefs);
 		}
+	}
+
+	public function contains (string $key) : bool {
+		return isset($this->prefs[$key]);
+	}
+
+	public function get (string $key) {
+		if (!isset($this->prefs[$key])) {
+			return null;
+		}
+
+		return $this->prefs[$key];
+	}
+
+	public function put (string $key, $value, bool $auto_save = false) {
+		$contains_key = $this->contains($key);
+
+		$this->prefs[$key] = $value;
+
+		if ($auto_save) {
+			//update database
+			Database::getInstance()->execute("INSERT INTO `{praefix}preferences` (
+				`key`, `area`, `value`
+			) VALUES (
+				:key, :area, :value
+			) ON DUPLICATE KEY UPDATE `value` = :value; ", array(
+				'key' => $key,
+				'area' => $this->area,
+				'value' => serialize($value)
+			));
+		} else {
+			$this->changed_prefs[$key] = $contains_key;
+		}
+	}
+
+	/**
+	 * write all values to database
+	 */
+	public function save () {
+		$lines = array();
+		$values = array();
+
+		$values['area'] = $this->area;
+
+		$i = 1;
+
+		foreach ($this->changed_prefs as $key=>$contains_key) {
+			$lines[] = "(:key" . $i . ", :area, :value" . $i . ")";
+			$values['key' . $i] = $key;
+			$values['value' . $i] = serialize($this->prefs[$key]);
+
+			$i++;
+		}
+
+		$lines_str = implode(",\r\n");
+
+		Database::getInstance()->execute("INSERT INTO `{praefix}preferences` (
+			`key`, `area`, `value`
+		) VALUES " . $lines_str . " ON DUPLICATE KEY UPDATE `value` = VALUES(value); ", $values);
 	}
 
 }
