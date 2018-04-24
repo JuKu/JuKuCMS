ce0d9de57e3a512f7836c198fccfb2519605ee74
JuKu <kuenzel.justin@t-online.de>
2018-04-24 22:25:01 +0200

    changed options of wysiwyg editor for sendmail page.

--- system/packages/com.jukusoft.cms.tools/classes/sendmailpage.php
+++ system/packages/com.jukusoft.cms.tools/classes/sendmailpage.php
@@ -48,7 +48,15 @@ class SendMailPage extends PageType {
 				<script>
 				  $(function () {
 					//Add text editor
-					$(\"#compose-textarea\").wysihtml5();
+					$(\"#compose-textarea\").wysihtml5({
+						\"font-styles\": true, //Font styling, e.g. h1, h2, etc. Default true
+						\"emphasis\": true, //Italics, bold, etc. Default true
+						\"lists\": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+						\"html\": true, //Button which allows you to edit the generated HTML. Default false
+						\"link\": true, //Button to insert a link. Default true
+						\"image\": false, //Button to insert an image. Default true,
+						\"color\": true //Button to change color of font  
+					});
 				  });
 				</script>";
 	}
