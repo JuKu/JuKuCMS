b48cc04e9786e0300bf55d58de70dea17dd86331
JuKu <kuenzel.justin@t-online.de>
2018-05-16 01:10:18 +0200

    changed class LDAPClient, see issue #25 .

--- system/packages/com.jukusoft.cms.ldap/classes/ldapclient.php
+++ system/packages/com.jukusoft.cms.ldap/classes/ldapclient.php
@@ -215,7 +215,7 @@ class LDAPClient {
 
 		$entries = ldap_get_entries($this->conn, $result);
 
-		if (count($entries)) {
+		if (count($entries) == 0) {
 			throw new IllegalStateException("user (uid=" . $user . ") not found in ldap server.");
 		}
 
