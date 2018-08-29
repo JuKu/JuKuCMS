fd739c97f20ffb777acc9210dfcb1b28b66cada9
juku <kuenzel.justin@t-online.de>
2018-08-29 16:55:54 +0200

    changed datatype boolean.

--- system/core/datatype/boolean.php
+++ system/core/datatype/boolean.php
@@ -30,23 +30,9 @@ class DataType_Boolean extends DataType_Base {
 	public function getFormCode(): string {
 		//return "<input type=\"checkbox\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getTitle() . "\"" . ($this->getValue() == true ? " checked" : "") . " />";
 
-		/*return "	<label>
+		return "	<label>
 						<input type=\"checkbox\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getTitle() . "\" class=\"flat-red\"" . ($this->getValue() == true ? " checked" : "") . " />
-                	</label>";*/
-
-		return "<!-- checkbox -->
-                <div class=\"form-group\">
-                  <label>
-                    <input type=\"checkbox\" class=\"flat-red\" checked>
-                  </label>
-                  <label>
-                    <input type=\"checkbox\" class=\"flat-red\">
-                  </label>
-                  <label>
-                    <input type=\"checkbox\" class=\"flat-red\" disabled>
-                    Flat green skin checkbox
-                  </label>
-                </div>";
+                	</label>";
 	}
 
 	public function validate(): bool {
