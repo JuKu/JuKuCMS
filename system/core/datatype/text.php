aa5bf7aac4451c3d67d8cae357a2a10c59a05e9a
juku <kuenzel.justin@t-online.de>
2018-08-29 14:56:48 +0200

    added datatype text.

---
+++ system/core/datatype/text.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Copyright (c) 2018 Justin Kuenzel (jukusoft.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * Project: RocketCMS
+ * License: Apache 2.0 license
+ * User: Justin
+ * Date: 29.08.2018
+ * Time: 14:56
+ */
+
+class DataType_Text implements DataType_Base {
+
+	public function load(array $row, $datatype_params) {
+		// TODO: Implement load() method.
+	}
+
+	public function getFormCode(): string {
+		// TODO: Implement getFormCode() method.
+	}
+
+	public function validate(): bool {
+		// TODO: Implement validate() method.
+	}
+
+}
+
+?>
