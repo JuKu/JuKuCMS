dbdc3e8bbddc68ddb2de21179f650cd297d7ce69
juku <kuenzel.justin@t-online.de>
2018-08-30 22:53:31 +0200

    changed css class of some settings form elements.

--- system/core/datatype/mail.php
+++ system/core/datatype/mail.php
@@ -28,7 +28,7 @@
 class DataType_Mail extends DataType_Base {
 
 	public function getFormCode(): string {
-		return "<input type=\"email\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getValue() . "\" />";
+		return "<input type=\"email\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getValue() . "\" class=\"form-control\" />";
 	}
 
 	public function validate(string $value): bool {
