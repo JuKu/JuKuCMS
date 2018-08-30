f0fccb2ec748720d36d16b8a8bc1781932bbd1be
juku <kuenzel.justin@t-online.de>
2018-08-30 18:23:20 +0200

    changed url pattern.

--- system/core/datatype/url.php
+++ system/core/datatype/url.php
@@ -28,7 +28,7 @@
 class DataType_URL extends DataType_Base {
 
 	public function getFormCode(): string {
-		return "<input type=\"url\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getValue() . "\" pattern=\"(https?:\/\/.*|#)\" placeholder=\"https://example.com\" />";
+		return "<input type=\"url\" name=\"" . $this->getInputName() . "\" value=\"" . $this->getValue() . "\" required aria-required=\"true\" pattern=\"(https?:\/\/.*|#)\" placeholder=\"https://example.com\" />";
 	}
 
 	public function validate(): bool {
