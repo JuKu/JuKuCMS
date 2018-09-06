81867984d9fcefb894d1632dc4f20bcd59da7bb6
juku <kuenzel.justin@t-online.de>
2018-09-06 17:04:09 +0200

    changed class PageEditPage, configured tinyMCE editor.

--- system/packages/com.jukusoft.cms.admin/classes/pageeditpage.php
+++ system/packages/com.jukusoft.cms.admin/classes/pageeditpage.php
@@ -208,7 +208,14 @@ class PageEditPage extends PageType {
 			</script>";*/
 
 		return "<script src=\"" . $thirdparty_url . "tinymce_4.8.2/js/tinymce/tinymce.min.js\"></script>
-  				<script>tinymce.init({ selector:'textarea' });</script>";
+  				<script>tinymce.init({
+					  selector: 'textarea',
+					  height: 500,
+					  theme: 'modern',
+					  plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+					  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+					  image_advtab: true
+				});</script>";
 	}
 
 	public function listRequiredPermissions(): array {
