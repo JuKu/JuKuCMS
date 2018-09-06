f8e9f39c4c2f009fb48be5aa66fdfd01ab2c1a42
juku <kuenzel.justin@t-online.de>
2018-09-06 18:50:16 +0200

    fixed some problems in class PageEditPage, removed fullpage plugin in tinyMCE editor.

--- system/packages/com.jukusoft.cms.admin/classes/pageeditpage.php
+++ system/packages/com.jukusoft.cms.admin/classes/pageeditpage.php
@@ -212,6 +212,7 @@ class PageEditPage extends PageType {
 					  selector: 'textarea',
 					  height: 500,
 					  theme: 'modern',
+					  removed_menuitems: 'newdocument',
 					  plugins: 'print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
 					  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
 					  image_advtab: true
